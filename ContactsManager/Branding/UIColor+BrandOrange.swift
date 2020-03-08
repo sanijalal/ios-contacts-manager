79d6b54d739b514407b5b282447d20136605eace
sanijalal <sani.jalal@iflix.com>
2020-03-08 10:08:55 +0800

    Fixed orange definition.

--- ContactsManager/Branding/UIColor+BrandOrange.swift
+++ ContactsManager/Branding/UIColor+BrandOrange.swift
@@ -1,5 +1,5 @@
 import UIKit
 
 extension UIColor {
-    static let brandOrange = UIColor(red: 255.0, green: 140.0, blue: 0.0, alpha: 1.0)
+    static let brandOrange = UIColor(red: 255.0/255.0, green: 140.0/255.0, blue: 0.0, alpha: 1.0)
 }
