d1b8f678cce553f813834884e34d3a648ebc6aa5
sanijalal <sani.jalal@iflix.com>
2020-03-08 09:39:59 +0800

    Added circle view. Added custom brand orange extension.

---
+++ ContactsManager/Branding/CircleView.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+@IBDesignable
+
+class CircleView: UIView {
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(ovalIn: CGRect(x: 4, y: 4, width: rect.width - 8, height: rect.height - 8))
+        UIColor.brandOrange.setFill()
+      path.fill()
+    }
+}
---
+++ ContactsManager/Branding/UIColor+BrandOrange.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+extension UIColor {
+    static let brandOrange = UIColor(red: 255.0, green: 140.0, blue: 0.0, alpha: 1.0)
+}
