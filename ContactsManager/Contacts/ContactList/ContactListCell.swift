968bd02ca654578c61deda8fdf6f8e4b9a1ac640
Sani <binabdjalal@gmail.com>
2021-09-05 13:56:29 +0800

    Removed unneeded override in ContactListCell.

--- ContactsManager/Contacts/ContactList/ContactListCell.swift
+++ ContactsManager/Contacts/ContactList/ContactListCell.swift
@@ -10,18 +10,7 @@ import UIKit
 
 class ContactListCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
     func configureCell(with contact: Contact) {
         nameLabel.text = contact.firstName + " " + contact.lastName
     }
